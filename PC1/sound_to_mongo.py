fe5dcaaf22d9251df53d5c13bbf9d0ab7ac33e89
ConchaCF <ccotrimferreira@gmail.com>
2026-04-26 16:06:33 +0100

    Esqueleto dos scripts para o mongo da nuvem

---
+++ PC1/sound_to_mongo.py
@@ -0,0 +1,30 @@
+import paho.mqtt.client as mqtt
+import pymongo
+import validacoes as v
+
+def receive_msg(client, userdata, message):
+    if(v.sound_anomalo()): #ver se e anomolo
+        registo= {""}
+        colecao= bd["sensor_errors"]
+    elif(v.sound_outlier()): #ver se e outlier
+        registo= {""}
+        colecao= bd["outliers"]
+    else: #cc e um valor valido
+        registo= {""}
+        colecao= bd["sounds_received"]
+
+    colecao.insert_one(registo)
+
+##################Codigo Principal##################
+#cliente Mongo
+mongo_cliente= pymongo.MongoClient("") #q endereco e q usamos?
+bd= mongo_cliente["SensorData"] #nome da base de dados
+
+#cliente MQTT
+mqtt_cliente= mqtt.Client("sound_mongo")
+mqtt_cliente.on_message= receive_msg
+
+mqtt_cliente.connect("www.hivemq.com", 1883)
+mqtt_cliente.loop_start()
+
+mqtt_cliente.subscribe("pisid_mazesound_4")
---
+++ PC1/validacoes.py
@@ -0,0 +1,17 @@
+def temp_anomalo():
+    return False
+
+def sound_anomalo():
+    return False
+
+def move_anomalo():
+    return False
+
+def temp_outlier():
+    return False
+
+def sound_outlier():
+    return False
+
+def move_outlier():
+    return False
