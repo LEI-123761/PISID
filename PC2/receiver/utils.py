be9f41d8f04ed3406fe43a942a200ff0464cb8d7
Misael Armando <manmisa2003@gmail.com>
2026-04-30 02:31:53 +0100

    Add better mysql connection; docker implementation

---
+++ PC2/receiver/utils.py
@@ -0,0 +1,25 @@
+# Moves
+MOVES_USER= "movimentos_user"
+MOVES_PASSWORD="movimentos_password"
+
+# Temp 
+TEMPS_USER="temperatura_user"
+TEMPS_PASSWORD="temperatura_password"
+
+# Sound 
+SOUNDS_USER="som_user"
+SOUNDS_PASSWORD="som_password"
+
+# Mysql DB
+HOST="mysql"
+DATABASE="maze"
+MYSQL_ATTEMPTS = 5
+
+# MQTT
+MQTT_BROKER = "broker.hivemq.com"
+MQTT_PORT   = 1883
+
+def get_id_simulacao(cursor):
+    cursor.execute("SELECT IDSimulacao FROM Simulacao WHERE Status='Correr' LIMIT 1")
+    row = cursor.fetchone() 
+    return row[0] if row else None
