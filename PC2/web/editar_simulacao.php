86738bc7166108dc722634e14b12b2050bbb9fb5
iris-baptista <alex2005iris@gmail.com>
2026-05-12 16:11:58 +0100

    adição páginas php (web ainda incompleto)

--- PC2/web/editar_simulacao.php
+++ PC2/web/editar_simulacao.php
@@ -5,8 +5,8 @@ if (!isset($_SESSION['IDUtilizador'])) {
     exit();
 }
 
-$user_id = $_SESSION['IDUtilizador'];
 $simulacao_id = $_GET['id'];
+$user_id = $_SESSION['IDUtilizador'];
 $username = $_SESSION['Email'];
 $password = $_SESSION['Password'];
 
@@ -27,20 +27,70 @@ if ($dados['Status'] === 'Correr') {
     exit();
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['btnGuardar'])) {
-    $nova_descricao = $_POST['descricao'];
-    $sql = "CALL Alterar_jogo(?, ?)";
-    $stmt = $ligacao->prepare($sql);
-    $stmt->bind_param("is", $simulacao_id, $nova_descricao);
+$sql2 = "SELECT LimiarTemperatura, LimiarSom, LimiarAlertaTemperatura, LimiarAlertaSom FROM Parametros WHERE IDSimulacao = ?";
+$stmt2 = $ligacao->prepare($sql2);
+$stmt2->bind_param("i", $simulacao_id);
+$stmt2->execute();
+$dados2 = $stmt2->get_result()->fetch_assoc();
 
-    if ($stmt->execute()) {
-        $stmt->close();
-        $ligacao->close();
-        header("Location: simulacoes.php");
-        exit();
-    } else {
-        $error = "Erro ao executar procedure: " . $ligacao->error;
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if(!empty($_POST['Descricao'])){
+        $campo='Descricao';
+        $stmt = $ligacao->prepare("CALL Alterar_jogo(?, ?)");
+        $stmt->bind_param("ss", $campo, $_POST['Descricao']);
+        if ($stmt->execute()) {
+            $stmt->close();
+        } else {
+            $error = "Erro ao executar procedure: " . $ligacao->error;
+        }
     }
+
+    if(!empty($_POST['LimiarTemperatura'])){
+        $campo='LimiarTemperatura';
+        $stmt = $ligacao->prepare("CALL Alterar_jogo(?, ?)");
+        $stmt->bind_param("ss", $campo, $_POST['LimiarTemperatura']);
+        if ($stmt->execute()) {
+            $stmt->close();
+        } else {
+            $error = "Erro ao executar procedure: " . $ligacao->error;
+        }
+    }
+
+    if(!empty($_POST['LimiarSom'])){
+        $campo='LimiarSom';
+        $stmt = $ligacao->prepare("CALL Alterar_jogo(?, ?)");
+        $stmt->bind_param("ss", $campo, $_POST['LimiarSom']);
+        if ($stmt->execute()) {
+            $stmt->close();
+        } else {
+            $error = "Erro ao executar procedure: " . $ligacao->error;
+        }
+    }
+
+    if(!empty($_POST['LimiarAlertaTemperatura'])){
+        $campo='LimiarAlertaTemperatura';
+        $stmt = $ligacao->prepare("CALL Alterar_jogo(?, ?)");
+        $stmt->bind_param("ss", $campo, $_POST['LimiarAlertaTemperatura']);
+        if ($stmt->execute()) {
+            $stmt->close();
+        } else {
+            $error = "Erro ao executar procedure: " . $ligacao->error;
+        }
+    }
+
+    if(!empty($_POST['LimiarAlertaSom'])){
+        $campo='LimiarAlertaSom';
+        $stmt = $ligacao->prepare("CALL Alterar_jogo(?, ?)");
+        $stmt->bind_param("ss", $campo, $_POST['LimiarAlertaSom']);
+        if ($stmt->execute()) {
+            $stmt->close();
+        } else {
+            $error = "Erro ao executar procedure: " . $ligacao->error;
+        }
+    }
+
+    header("Location: simulacoes.php");
+    exit();
 }
 ?>
 
@@ -49,23 +99,48 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['btnGuardar'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Editar Simulação</title>
-    <link rel="stylesheet" href="css/editar_simulacao.css">
+    <title>Editar Simulação <?php echo $simulacao_id ?></title>
+    <link rel="stylesheet" href="css/nova_simulacao.css">
 </head>
+
 
 <body>
 <div class="container">
-    <h2>Editar Simulação <?php echo $dados['IDSimulacao']; ?></h2>
-    <div>
-        <form class="edit-box" method="POST">
-            <label for="descricao">Descrição</label>
-            <input id="descricao" name="descricao" value="<?php echo $dados['Descricao']; ?>">
-            <div class="button-container">
-                <button type="button" class="cancel" onclick="window.location.href='simulacoes.php'">Cancelar</button>
-                <button type="submit" name="btnGuardar">Guardar</button>
-            </div>
-        </form>
+    <div class="header-container">
+        <h2 class="title">Editar Simulação <?php echo $simulacao_id ?>:</h2>
     </div>
+    <form class="edit-box" method="POST">
+        <div class="form-grid">
+            <div class="input-group-desc">
+                <label for="descricao">Descrição</label>
+                <input type="text" id="descricao" name="descricao" value="<?php echo htmlspecialchars($dados['Descricao']); ?>">
+            </div>
+            <div class="input-group">
+                <label for="limTemp">Limiar Temperatura</label>
+                <input type="number" id="limTemp" name="limTemp" step="0.01" min="0.00" value="<?php echo $dados2['LimiarTemperatura']; ?>">
+            </div>
+
+            <div class="input-group">
+                <label for="limAlertTemp">Limiar Alerta Temperatura</label>
+                <input type="number" id="limAlertTemp" name="limAlertTemp" step="0.01" min="0.00" value="<?php echo $dados2['LimiarAlertaTemperatura']; ?>">
+            </div>
+
+            <div class="input-group">
+                <label for="limSom">Limiar Som</label>
+                <input type="number" id="limSom" name="limSom" step="0.01" min="0.00" value="<?php echo $dados2['LimiarSom']; ?>">
+            </div>
+
+            <div class="input-group">
+                <label for="limAlertSom">Limiar Alerta Som</label>
+                <input type="number" id="limAlertSom" name="limAlertSom" step="0.01" min="0.00" value="<?php echo $dados2['LimiarAlertaSom']; ?>" required>
+            </div>
+        </div>
+        <div class="button-container">
+            <button type="button" class="cancel" onclick="window.location.href='simulacoes.php'">Cancelar</button>
+            <button type="submit" class="save" name="btnGuardar">Guardar</button>
+        </div>
+    </form>
+
 </div>
 
 </body>
